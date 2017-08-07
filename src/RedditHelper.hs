fe368d8bf5f12cffb42b453c724a6ffa5de8135d
Gizmo Mk. 0 <gizmo.mk0@gmail.com>
2017-08-07 22:02:11 +0200

    Added docker support; Changed the CSS class for flairing

--- src/RedditHelper.hs
+++ src/RedditHelper.hs
@@ -42,10 +42,10 @@ executeTask (PostEntry ne) = do
   newPostId <- submitLink wikinewsName
                           (T.toStrict . T.pack . take 300 $ newsTitle ne)
                           (T.toStrict . T.pack $ url ne)
-  setPostFlair wikinewsName newPostId (T.toStrict . T.pack $ newsCategory ne) (T.toStrict . T.pack $ newsCategory ne)
+  setPostFlair wikinewsName newPostId (T.toStrict . T.pack $ newsCategory ne) (T.toStrict . T.pack . head . words $ newsCategory ne)
 executeTask (MarkDeleted re) = do
   liftIO $ putStrLn "Marking deleted"
   setPostFlair wikinewsName (postId re) (T.toStrict . T.pack $ "Deleted") (T.toStrict . T.pack $ "deleted")
 executeTask (ChangeFlair re nc) = do
   liftIO $ putStrLn "Changing flair"
-  setPostFlair wikinewsName (postId re) (T.toStrict . T.pack $ nc) (T.toStrict . T.pack $ nc)
+  setPostFlair wikinewsName (postId re) (T.toStrict . T.pack $ nc) (T.toStrict . T.pack . head . words $ nc)
